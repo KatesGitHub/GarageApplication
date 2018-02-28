d038bab9c294ffb2359a757db981d67e07d63008
Omperkash Maheshwari <omperkash.maheshwari1981@gmail.com>
2018-02-28 13:48:03 +0100

    Modify class

--- src/models/Vehicle.java
+++ src/models/Vehicle.java
@@ -2,4 +2,9 @@ package models;
 
 public abstract class Vehicle {
 
+	private String name;
+	private String regNumber;
+	private double weight;
+	private double length;
+	private int numberOfSeats;
 }
